55f27f2191e527e89fadf075d8b4b2a5fff64f6e
Kevin Wiegand <kwiegand@Gazprom-mt.com>
2016-09-01 02:04:50 +0100

    using fast-csv instead

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 console.log('About to import into elastic');
-var elasticsearchCsv = require('elasticsearch-csv');
-// create an instance of the importer with options 
-var esCSV = new elasticsearchCsv({
-    es: { index: 'ethos_index', type: 'ethos_health_data', host: 'localhost:9200' },
-    csv: { filePath: 'C:\\EthosData\\BioAge_Results_Sample_Data.csv', headers: true }
-});
-esCSV.import()
-    .then(function (response) {
-    // Elasticsearch response for the bulk insert 
-    console.log(response);
-}, function (err) {
-    // throw error 
-    throw err;
+var csv = require("fast-csv");
+csv
+    .fromPath("C:\\EthosData\\BioAge_Results_Sample_Data.csv")
+    .on("data", function (data) {
+    console.log(data);
+})
+    .on("end", function () {
+    console.log("done");
 });
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,18 +1,14 @@
 ﻿console.log('About to import into elastic');
 
-var elasticsearchCsv = require('elasticsearch-csv');
 
-// create an instance of the importer with options 
-var esCSV = new elasticsearchCsv({
-    es: { index: 'ethos_index', type: 'ethos_health_data', host: 'localhost:9200' },
-    csv: { filePath: 'C:\\EthosData\\BioAge_Results_Sample_Data.csv', headers: true }
-});
+var csv = require("fast-csv");
 
-esCSV.import()
-    .then(function (response) {
-        // Elasticsearch response for the bulk insert 
-        console.log(response);
-    }, function (err) {
-        // throw error 
-        throw err;
+csv
+    .fromPath("C:\\EthosData\\BioAge_Results_Sample_Data.csv")
+    .on("data", function (data) {
+        console.log(data);
+    })
+    .on("end", function () {
+        console.log("done");
     });
+
